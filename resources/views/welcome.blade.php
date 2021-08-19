b6ec7d68c40b9a2e7f956895e5f0734617ca8f82
TamNguyen <ngductam91@github.com>
2021-08-19 09:41:30 +0700

    update hoobysurvey

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
             background-color: #fff;
             color: #636b6f;
             font-family: "Comic Sans MS", "Comic Sans", cursive;
-            font-size:100px;
+            font-size: 100px;
             height: 50vh;
             margin: 0;
             align-items: center;
@@ -13,18 +13,21 @@
             justify-content: center;
         }
 
-        label{
+        label {
             background-color: #fff;
             color: #636b6f;
             font-family: "Comic Sans MS", "Comic Sans", cursive;
-            font-size:25px;
+            font-size: 25px;
         }
     </style>
     <p>Welcome to survey</p>
     <form method="get" action="{{route('createEmail')}}">
         <div class="form-group" style="text-align: center">
             <label> Insert your email to begin
-            <input type="email" class="form-control" placeholder="Email" name="email" required>
+                <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" required>
+                @error('email')
+                <p class="text-warning">{{$message}}</p>
+                @enderror
             </label>
             <button type="submit" class="btn btn-primary">Begin</button>
         </div>
